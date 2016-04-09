93700ab27d81666a8c413135f2e7fc349aec5f97
Thiago Oliveira <thideoli@gmail.com>
2016-04-09 13:17:09 -0300

    Start

---
+++ app/src/main/java/com/example/pedro/gdgsjcvirus/StatusResponse.java
@@ -0,0 +1,11 @@
+package com.example.pedro.gdgsjcvirus;
+
+
+import javax.net.ssl.SSLEngineResult;
+
+/**
+ * Created by SAEMI on 09/04/2016.
+ */
+public interface StatusResponse {
+    SSLEngineResult.Status getStatus();
+}
